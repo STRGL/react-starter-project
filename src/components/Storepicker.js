2dc1865e768c3ba8c17fecfc57b5971194f4d438
STRGL <daryllclarke@hotmail.co.uk>
2020-04-14 13:29:19 +0100

    Add css to index file

--- src/components/Storepicker.js
+++ src/components/Storepicker.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 class StorePicker extends React.Component {
-    render(){
+    render() {
         return (
-            <form className="store-seletctor">
+            <form className="store-selector">
                 <h2>Please Enter A Store</h2>
-                <input type="text" required placeholder="Store Name"/>
+                <input type="text" required placeholder="Store Name" />
                 <button type="submit">Visit Store</button>
             </form>
         )
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 import StorePicker from './components/Storepicker';
+import "./css/style.css";
 // export default StorePicker;
 render(<StorePicker />, document.querySelector('#main'));
